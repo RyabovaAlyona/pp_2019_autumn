d123c334a7dd19697b24be9ee26d6811615cc1ae
Alyona <ryabova.alena99@gmail.com>
2019-10-12 12:44:59 +0300

    done

---
+++ modules/task_1/ryabova_a_count_frequency_char_in_line/count_frequency.h
@@ -0,0 +1,11 @@
+// Copyright 2019 Ryabova Alyona
+#ifndef MODULES_TASK_1_RYABOVA_A_COUNT_FREQUENCY_CHAR_IN_LINE_
+#define MODULES_TASK_1_RYABOVA_A_COUNT_FREQUENCY_CHAR_IN_LINE_
+
+#include <string>
+#include <iostream>
+
+int getCount(std::string str, char c);
+int getCountCharInStr(std::string str, char ch);
+
+#endif  // MODULES_TASK_1_RYABOVA_A_COUNT_FREQUENCY_CHAR_IN_LINE_
