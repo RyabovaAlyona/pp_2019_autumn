209445c0c5da961556a4d2c52f1fe8efe6ff56dc
Alyona <ryabova.alena99@gmail.com>
2019-10-12 14:38:40 +0300

    fix after travis

--- modules/task_1/ryabova_a_count_frequency_char_in_line/main.cpp
+++ modules/task_1/ryabova_a_count_frequency_char_in_line/main.cpp
@@ -6,81 +6,86 @@
 #include "./count_frequency.h"
 
 TEST(count_frequency, even_number_of_char) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	std::string str = "ariana";
-	char c = 'a';
+    std::string str = "ariana";
+    char c = 'a';
 
-	int count = getCountCharInStr(str, c);
+    int count = getCountCharInStr(str, c);
 
-	if (rank == 0)
-		ASSERT_EQ(3, count);
+    if (rank == 0) {
+        ASSERT_EQ(3, count);
+    }
 }
 
 TEST(count_frequency, odd_number_of_char) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	std::string str = "arianadad";
-	char c = 'a';
+    std::string str = "arianadad";
+    char c = 'a';
 
-	int count = getCountCharInStr(str, c);
+    int count = getCountCharInStr(str, c);
 
-	if (rank == 0)
-		ASSERT_EQ(4, count);
+    if (rank == 0) {
+        ASSERT_EQ(4, count);
+    }
 }
 
 TEST(count_frequency, all_char_needed) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	std::string str = "aaaaaaaaaaa";
-	char c = 'a';
+    std::string str = "aaaaaaaaaaa";
+    char c = 'a';
 
-	int count = getCountCharInStr(str, c);
+    int count = getCountCharInStr(str, c);
 
-	if (rank == 0)
-		ASSERT_EQ(11, count);
+    if (rank == 0) {
+        ASSERT_EQ(11, count);
+    }
 }
 
 TEST(count_frequency, no_char_needed) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	std::string str = "sdfghjkl";
-	char c = 'a';
+    std::string str = "sdfghjkl";
+    char c = 'a';
 
-	int count = getCountCharInStr(str, c);
+    int count = getCountCharInStr(str, c);
 
-	if (rank == 0)
-		ASSERT_EQ(0, count);
+    if (rank == 0) {
+        ASSERT_EQ(0, count);
+    }
 }
 
 TEST(count_frequency, number_search) {
-	int rank;
-	MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    int rank;
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
 
-	std::string str = "sdfghjkl12421";
-	char c = '1';
+    std::string str = "sdfghjkl12421";
+    char c = '1';
 
-	int count = getCountCharInStr(str, c);
+    int count = getCountCharInStr(str, c);
 
-	if (rank == 0)
-		ASSERT_EQ(2, count);
+    if (rank == 0) {
+        ASSERT_EQ(2, count);
+    }
 }
 
 int main(int argc, char** argv) {
-	::testing::InitGoogleTest(&argc, argv);
-	MPI_Init(&argc, &argv);
+    ::testing::InitGoogleTest(&argc, argv);
+    MPI_Init(&argc, &argv);
 
-	::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
-	::testing::TestEventListeners& listeners =
-		::testing::UnitTest::GetInstance()->listeners();
+    ::testing::AddGlobalTestEnvironment(new GTestMPIListener::MPIEnvironment);
+    ::testing::TestEventListeners& listeners =
+        ::testing::UnitTest::GetInstance()->listeners();
 
-	listeners.Release(listeners.default_result_printer());
-	listeners.Release(listeners.default_xml_generator());
+    listeners.Release(listeners.default_result_printer());
+    listeners.Release(listeners.default_xml_generator());
 
-	listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
-	return RUN_ALL_TESTS();
+    listeners.Append(new GTestMPIListener::MPIMinimalistPrinter);
+    return RUN_ALL_TESTS();
 }
